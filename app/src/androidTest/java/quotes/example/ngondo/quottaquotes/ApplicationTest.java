31455d175be1d6317fd174e862f63adb2067c56f
Cornellius Ngondo <devngondo@gmail.com>
2015-04-22 19:17:07 +0000

    Initial commit with imported libs

---
+++ app/src/androidTest/java/quotes/example/ngondo/quottaquotes/ApplicationTest.java
@@ -0,0 +1,13 @@
+package quotes.example.ngondo.quottaquotes;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
